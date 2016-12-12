4130ccf662e25f3a2e620afd9f58d2dd2511dc60
PetitGrigri <fgriselles@hotmail.fr>
2016-12-12 22:57:00 +0100

    Mise à jour projet TableView

--- Exemple_TableView/Exemple_TableView/Exemple_TableView/FruitsTableTableViewController.swift
+++ Exemple_TableView/Exemple_TableView/Exemple_TableView/FruitsTableTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FruitsTableTableViewController: UITableViewController {
 
+    //variable contenant des fruits
     private  var fruits = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
                   "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
                   "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
@@ -25,32 +26,55 @@ class FruitsTableTableViewController: UITableViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
+    //Méthode indiquant le nombre de section de notre table view
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return fruits.count
-    }
-
     
+    //Méthode indiquant le nombre de fruit pour la section
+    //attention le nombre de row est lié à une section
+    //ici la première section aura 12 image et la seconde 16
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section == 0) {
+            return 12
+        } else {
+            return 16
+        }
+
+    }
+
+    //Méthode permettant de créer ou de renvoyer une cellule
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //récupération de notre cellule (cette dernière se trouve dans notre story board avec l'identifiant labelCell
+        //si on avait pas fait comme ca on aurait du (sauf erreur créer à la main notre cellule)
         let cell = tableView.dequeueReusableCell(withIdentifier: "labelCell", for: indexPath)
+        
+        //récupération du nom du fuit à afficher
+        let fruitName = fruits[indexPath.row + ((indexPath.section == 0) ? 0 : 12 ) ]
 
-        cell.textLabel?.text = " \(fruits[indexPath.row])"
+        //remplissage du texte du fruit ainsi que du sous titre
+        cell.detailTextLabel?.text = "Delicious!"
+        cell.textLabel?.text = fruitName
 
+        //on set ici l'image (si cette image existe (ici les images se trouve dans le dossier Assets.xcassets)
+        if let imageFruit = UIImage(named: fruitName) {
+            cell.imageView?.image = imageFruit                                  //on set l'image
+            cell.imageView?.frame = CGRect(x:0.0,y:0.0,width:40.0,height:40.0)  //on set la taille de l'image
+            cell.imageView?.layer.cornerRadius = 22                             //on affiche l'image dans un frame "ronde"
+            cell.imageView?.clipsToBounds = true                                //on coupe l'image qui dépasse de cette frame
+            cell.imageView?.layer.borderWidth = 2                               //taille de la bordure
+            cell.imageView?.layer.borderColor = UIColor.init(red: 0.95, green: 0.95, blue: 1, alpha: 1).cgColor //couleur de la bordure
+        }
+        
+        //retour de la cellule
         return cell
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Fruits"
+        return "Fruits (section \(section + 1))"
     }
 
     /*
