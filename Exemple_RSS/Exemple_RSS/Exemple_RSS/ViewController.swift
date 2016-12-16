61c5f71919bdf6852fc5036fac5b2b3ba5783d83
PetitGrigri <fgriselles@hotmail.fr>
2016-12-16 22:11:21 +0100

    Initialisation Projet Exemple_RSS
    Création du projet

---
+++ Exemple_RSS/Exemple_RSS/Exemple_RSS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exemple_RSS
+//
+//  Created by Fabien on 16/12/2016.
+//  Copyright © 2016 PetitGrigri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
